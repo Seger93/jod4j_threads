ba4c92a8b12d7cff0ccaea9cc0934e5161859bc2
Admin <Serger9393@mail.ru>
2023-04-10 23:45:31 +0300

    8. Скачивание файла с ограничением. [#144271 #408391]

--- src/main/java/ru/job4j/concurrent/Wget.java
+++ src/main/java/ru/job4j/concurrent/Wget.java
@@ -9,23 +9,33 @@ import java.net.URL;
 public class Wget implements Runnable {
 
     private final String url;
-
+    private final String fileName;
     private final int speed;
 
-    public Wget(String url, int speed) {
+    public Wget(String url, String fileName, int speed) {
         this.url = url;
+        this.fileName = fileName;
         this.speed = speed;
     }
 
     @Override
     public void run() {
         try (BufferedInputStream in = new BufferedInputStream(new URL(url).openStream());
-             FileOutputStream fileOutputStream = new FileOutputStream("pom_tmp.xml")) {
+             FileOutputStream fileOutputStream = new FileOutputStream(fileName)) {
             byte[] dataBuffer = new byte[1024];
             int bytesRead;
-            while ((bytesRead = in.read(dataBuffer, 0, speed)) != -1) {
+            int downloadData = 0;
+            long startTimeStamp = System.currentTimeMillis();
+            while ((bytesRead = in.read(dataBuffer, 0, 1024)) != -1) {
+                downloadData += bytesRead;
+                if (downloadData >= speed) {
+                    long endTimeStamp = System.currentTimeMillis();
+                    if ((endTimeStamp - startTimeStamp) < 1000)
+                        Thread.sleep(1000 - (endTimeStamp - startTimeStamp));
+                    startTimeStamp = System.currentTimeMillis();
+                    downloadData = 0;
+                }
                 fileOutputStream.write(dataBuffer, 0, bytesRead);
-                Thread.sleep(1000);
             }
         } catch (IOException | InterruptedException e) {
             e.printStackTrace();
@@ -34,7 +44,10 @@ public class Wget implements Runnable {
     }
 
     public static void main(String[] args) {
-        if (!args[0].startsWith("https") && !args[0].endsWith(".xml")) {
+        if (args.length == 0) {
+            throw new IllegalArgumentException("Не заданы аргументы/пустой массив");
+        }
+        if (!args[0].startsWith("https")) {
             throw new IllegalArgumentException("Некорректное имя файла");
         }
         if (Integer.parseInt(args[1]) < 0) {
@@ -42,7 +55,8 @@ public class Wget implements Runnable {
         }
         String url = args[0];
         int speed = Integer.parseInt(args[1]);
-        Thread wget = new Thread(new Wget(url, speed));
+        String fileName = args[2];
+        Thread wget = new Thread(new Wget(url, fileName, speed));
         wget.start();
         try {
             wget.join();
