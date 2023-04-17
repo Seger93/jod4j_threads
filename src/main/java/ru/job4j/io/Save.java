9636d350dedea3dd69a790cf3a9a7d4b6e52f039
Admin <Serger9393@mail.ru>
2023-04-17 23:22:01 +0300

    1. Visibility. Общий ресурс вне критической секции [#1102]

---
+++ src/main/java/ru/job4j/io/Save.java
@@ -0,0 +1,21 @@
+package ru.job4j.io;
+
+import java.io.*;
+
+public class Save {
+    private final File file;
+
+    public Save(File file) {
+        this.file = file;
+    }
+
+    public void saveContent(String content) throws IOException {
+        try (BufferedOutputStream o = new BufferedOutputStream(new FileOutputStream(file))) {
+            for (int i = 0; i < content.length(); i += 1) {
+                o.write(content.charAt(i));
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+}
