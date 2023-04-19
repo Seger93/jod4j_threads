40fe7a3d22016fe247f8cbe10618ba6648e9998b
Admin <Serger9393@mail.ru>
2023-04-19 23:40:13 +0300

    4. ThreadSafe динамический список [#1105]

---
+++ src/main/java/ru/job4j/synch/SingleLockList.java
@@ -0,0 +1,40 @@
+package ru.job4j.synch;
+
+
+import net.jcip.annotations.GuardedBy;
+import net.jcip.annotations.ThreadSafe;
+
+import java.util.*;
+
+@ThreadSafe
+public class SingleLockList<T> implements Iterable<T> {
+    @GuardedBy("this")
+    private final List<T> list;
+
+    public SingleLockList(List<T> list) {
+        this.list = copy(list);
+    }
+
+    public synchronized void add(T value) {
+        list.add(value);
+    }
+
+    public synchronized T get(int index) {
+       return list.get(index);
+    }
+
+    @Override
+    public synchronized Iterator<T> iterator() {
+        return copy(list).iterator();
+    }
+    /**
+    *Возвращает синхронизированный (потокобезопасный) список, поддерживаемый указанным списком.
+    *Чтобы гарантировать последовательный доступ,
+    *очень важно, чтобы весь доступ к резервному списку выполнялся через возвращаемый список.
+    *Крайне важно, чтобы пользователь вручную синхронизировал возвращаемый список
+    * при его обходе через Iterator, Spliterator или Stream:
+     */
+    private synchronized List<T> copy(List<T> origin) {
+        return new LinkedList<>(Collections.synchronizedList(origin));
+    }
+}
