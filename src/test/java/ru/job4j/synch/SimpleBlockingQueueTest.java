c3c65c75b65538188d683b7505a3e5f745735dc6
Admin <Serger9393@mail.ru>
2023-04-25 22:11:42 +0300

    3. Junit тест для блокирующей очереди. [#68589]

--- src/test/java/ru/job4j/synch/SimpleBlockingQueueTest.java
+++ src/test/java/ru/job4j/synch/SimpleBlockingQueueTest.java
@@ -3,7 +3,9 @@ package ru.job4j.synch;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.concurrent.CopyOnWriteArrayList;
 
 class SimpleBlockingQueueTest {
@@ -42,5 +44,43 @@ class SimpleBlockingQueueTest {
         consumer.interrupt();
         consumer.join();
         Assertions.assertEquals(buffer, Arrays.asList(0, 1, 2, 3, 4));
+    }
+
+    @Test
+    public void whenOfferAndPoll() throws InterruptedException {
+        SimpleBlockingQueue<Integer> simpleBlockingQueue = new SimpleBlockingQueue<>(2);
+        List<Integer> expected = new ArrayList<>(List.of(1, 2, 3, 4));
+        List<Integer> actual = new ArrayList<>();
+        Thread producer = new Thread(
+                () -> {
+                    try {
+                        for (Integer integer : expected) {
+                            simpleBlockingQueue.offer(integer);
+                        }
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                        Thread.currentThread().interrupt();
+                    }
+
+                }
+        );
+        Thread consumer = new Thread(
+                () -> {
+                    try {
+                        actual.add(simpleBlockingQueue.poll());
+                        actual.add(simpleBlockingQueue.poll());
+                        actual.add(simpleBlockingQueue.poll());
+                        actual.add(simpleBlockingQueue.poll());
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                        Thread.currentThread().interrupt();
+                    }
+                }
+        );
+        producer.start();
+        consumer.start();
+        producer.join();
+        consumer.join();
+        Assertions.assertEquals(actual, expected);
     }
 }
